0c3b239b55921168fc590143220bee6f570e9524
Lilian Francine Bragaia Lacerda <lacerda.lilianfb@gmail.com>
2025-05-22 15:35:38 +0000

    mod

--- superTrunfoLogicaDesafios.c
+++ superTrunfoLogicaDesafios.c
@@ -1,67 +1,70 @@
 #include <stdio.h>
+
 int main() {
-    //Variaveis da carta 1
+    // Variaveis da carta 1
     char estado1;
     char codigo1[4];
-    char cidade1[50];
+    char nomeCidade1[50];
     int populacao1;
     float area1;
     float pib1;
     int pontosTuristicos1;
     float densidade1;
+    float pibPerCapita1;
 
-    //Variaveis da carta 2
+    // Variaveis da carta 2
     char estado2;
     char codigo2[4];
-    char cidade2[50];
+    char nomeCidade2[50];
     int populacao2;
     float area2;
     float pib2;
     int pontosTuristicos2;
     float densidade2;
+    float pibPerCapita2;
 
-    //Entrada e exibicao dos dados da carta 1
+    // Entrada e exibicao dos dados da carta 1
     printf("Carta 1:\n");
 
     printf("Estado:\n");
-    scanf("%c", &estado1);
+    scanf(" %c", &estado1);
 
     printf("Código:\n");
-    scanf("%s", &codigo1);
+    scanf("%s", codigo1);
 
     printf("Cidade:\n");
-    fgets(cidade1, 50, stdin);
+    fgets(nomeCidade1, 50, stdin);
 
     printf("População:\n");
     scanf("%d", &populacao1);
 
-    printf("Área (em km²:\n)");
+    printf("Área (em km²):\n");
     scanf("%f", &area1);
 
     printf("PIB (em bilhões de reais):\n");
     scanf("%f", &pib1);
 
-    printf("Número de pontos turísticos:\n");
+    printf("Número de Pontos Turísticos:\n");
     scanf("%d", &pontosTuristicos1);
 
-    //Entrada e exibicao dos dados da carta 2
-    printf("Carta 2:\n");
+    // Entrada e exibicao dos dados da carta 2
+    printf("\nCarta 2:\n");
 
     printf("Estado:\n");
-    scanf("%c", &estado2);
+    scanf(" %c", &estado2);
 
     printf("Código:\n");
-    scanf("%s", &codigo2);
+    scanf("%s", codigo2);
 
     printf("Cidade:\n");
-    fgets(cidade2, 50, stdin);
+    fgets(nomeCidade2, 50, stdin);
 
     printf("População:\n");
     scanf("%d", &populacao2);
 
     printf("Área (em km²):\n");
     scanf("%f", &area2);
-    
+
     printf("PIB (em bilhões de reais):\n");
     scanf("%f", &pib2);
 
@@ -92,7 +95,7 @@ int main() {
         printf("Resultado: Empate!\n");
     }
 
-        // Calculos
+    // Calculos
     densidade1 = populacao1 / area1;
     pibPerCapita1 = (pib1 * 1000000000) / populacao1;
 
@@ -100,5 +103,4 @@ int main() {
     pibPerCapita2 = (pib2 * 1000000000) / populacao2;
 
     return 0;
-
 }
